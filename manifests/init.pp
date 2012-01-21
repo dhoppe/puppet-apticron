2e6721452d18e6759459ad25658fa9f943431586
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-01-21 13:46:18 +0100

    Fix module layout

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,7 +25,7 @@ class apticron {
 			group   => root,
 			mode    => 0644,
 			alias   => "apticron.conf",
-			content => template("apticron/$::lsbdistcodename/etc/apticron/apticron.conf.erb"),
+			content => template("apticron/${::lsbdistcodename}/etc/apticron/apticron.conf.erb"),
 			require => Package["apticron"],
 		}
 	}
