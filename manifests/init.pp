57335e13bbeb8f64b0fe59eac483eed708e314a7
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-04-11 13:38:49 +0200

    Fix several issues detected by puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,62 +1,62 @@
 class apticron {
 	define listchanges::email($email = false) {
 		$t_email = $email ? {
-			false   => "root",
+			false   => 'root',
 			default => $email,
 		}
 
 		file { "$name":
 			owner   => root,
 			group   => root,
-			mode    => 0644,
-			content => template("apticron/common/etc/apt/listchanges.conf.erb"),
-			require => Package["apt-listchanges"],
-		}
-	 }
-
-	 define apticron::email($email = false) {
-		$t_email = $email ? {
-			false   => "root",
-			default => $email,
-		}
-
-		file { "$name":
-			owner   => root,
-			group   => root,
-			mode    => 0644,
-			alias   => "apticron.conf",
-			content => template("apticron/${::lsbdistcodename}/etc/apticron/apticron.conf.erb"),
-			require => Package["apticron"],
+			mode    => '0644',
+			content => template('apticron/common/etc/apt/listchanges.conf.erb'),
+			require => Package['apt-listchanges'],
 		}
 	}
 
-	listchanges::email { "/etc/apt/listchanges.conf":
+	define apticron::email($email = false) {
+		$t_email = $email ? {
+			false   => 'root',
+			default => $email,
+		}
+
+		file { "$name":
+			owner   => root,
+			group   => root,
+			mode    => '0644',
+			alias   => 'apticron.conf',
+			content => template("apticron/${::lsbdistcodename}/etc/apticron/apticron.conf.erb"),
+			require => Package['apticron'],
+		}
+	}
+
+	listchanges::email { '/etc/apt/listchanges.conf':
 		email => hiera('listchanges'),
 	}
 
-	apticron::email { "/etc/apticron/apticron.conf":
+	apticron::email { '/etc/apticron/apticron.conf':
 		email => hiera('apticron'),
 	}
 
-	file { "/etc/cron.d/apticron":
+	file { '/etc/cron.d/apticron':
 		owner   => root,
 		group   => root,
-		mode    => 0644,
-		content => template("apticron/common/etc/cron.d/apticron.erb"),
+		mode    => '0644',
+		content => template('apticron/common/etc/cron.d/apticron.erb'),
 		require => [
-			File["apticron.conf"],
-			Package["apticron"]
+			File['apticron.conf'],
+			Package['apticron']
 		],
 	}
 
-	file { "/etc/cron.daily/apticron":
+	file { '/etc/cron.daily/apticron':
 		ensure => absent,
 	}
 
 	package { [
-		"apticron",
-		"apt-listchanges",
-		"apt-show-versions" ]:
+		'apticron',
+		'apt-listchanges',
+		'apt-show-versions' ]:
 		ensure => present,
 	}
 }
