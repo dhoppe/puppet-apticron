e8cda69c56684b66c37888cdf14ff7d82ee6fa36
Vinicius Teles <vinicius@improveit.com.br>
2012-09-09 23:12:05 -0300

    Removed the dependence on hiera
    It might be just me, but I found this dependence to be a bit confusing
    and I couldn't really make it work out. I wonder if it's really
    necessary. Maybe it would be better if the code could fall back to
    something else when hiera isn't installed.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 class apticron::params {
   case $::lsbdistcodename {
     'lenny', 'squeeze', 'maverick', 'natty', 'precise': {
-      $apticron    = hiera('apticron')
-      $listchanges = hiera('listchanges')
+      $apticron    = ''
+      $listchanges = ''
     }
     default: {
       fail("Module ${module_name} does not support ${::lsbdistcodename}")
