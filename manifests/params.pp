14a5b4748a157995a3bd17d49bf0b4c743b3ba25
Vinicius Teles <vinicius@improveit.com.br>
2012-09-09 22:47:30 -0300

    added Ubuntu Precise to the list of supported distro codenames

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class apticron::params {
   case $::lsbdistcodename {
-    'lenny', 'squeeze', 'maverick', 'natty': {
+    'lenny', 'squeeze', 'maverick', 'natty', 'precise': {
       $apticron    = hiera('apticron')
       $listchanges = hiera('listchanges')
     }
